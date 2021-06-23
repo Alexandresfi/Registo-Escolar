eb958cae9b36aa98bc50a1352a261f83236bc348
Alexandre Nascimento <81328399+Alexandresfi@users.noreply.github.com>
2021-06-23 12:01:26 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-alert("ATENÇÃO: Estamos em desenvolvimento")
 var nomeAluno = document.getElementById("nome");
+
 
 var alunos = [];
 
@@ -16,6 +16,8 @@ function CriarAluno () {
             return (this.notas1[0] + this.notas2[0]+this.notas3[0])/3
         }
     }
+
+    
     alunos.push(Aluno);
     nomeAluno.focus(); 
     var {nome}=aluno;
@@ -25,6 +27,8 @@ function CriarAluno () {
 }
 
 function MostrarAluno() {
+
+    
 
     let tabela = document.getElementById("aqui");
     //Para faltas (de td1 a td3)
@@ -42,6 +46,7 @@ function MostrarAluno() {
     td2.textContent = nomeAluno.value;
     td1.textContent = pos+1;
     td3.innerHTML = '<input type="checkbox" name="falta" value="falta">';
+    
 
     //adicionando os elementos da tabela
     
@@ -49,8 +54,8 @@ function MostrarAluno() {
     tr.appendChild(td1);
     tr.appendChild(td2);
     tr.appendChild(td3);
-    console.log(pos);
     AddNotas()
+    AddRelatorio()
     nomeAluno.value ="";   
     nomeAluno.innerHTML=" ";
 }
@@ -68,9 +73,11 @@ function AddNotas(){
     var td6 = document.createElement("td");
 
     let pos = alunos.indexOf(aluno);
-        
+    
+    
     let numero = document.createTextNode(pos);
     td1.appendChild(numero);  
+
     
     //colocando os inpuit
     td3.innerHTML = `<input type= number name=notas class='nota ${pos}'  placeholder=nota>`
@@ -84,11 +91,12 @@ function AddNotas(){
         let nota3=parseInt(document.getElementsByClassName("nota "+ pos)[2].value);
         let media = (nota1+nota2+nota3)/3;
         td6.innerHTML = `<input type= number name=notas class='nota ${pos}'  placeholder=nota value = '${media}'>`
-        console.log(document.getElementsByClassName("nota "+ pos)[0].value)
     })
     
+
+
     //números e nomes dos alunos
-    td1.textContent = pos+1;
+    td1.textContent = pos +1;
     td2.textContent = nomeAluno.value;
     tabelaNota.appendChild(tr);
     tr.appendChild(td1);
@@ -98,6 +106,58 @@ function AddNotas(){
     tr.appendChild(td5);
     tr.appendChild(td6);
 }
+
+function AddRelatorio() {
+ //adicionando os números e os alunos no relatório 
+let tabelaRela = document.getElementsByClassName("aluno")[0];
+let tr = document.createElement("tr");
+    let td1 = document.createElement("td");
+    let td2 = document.createElement("td");
+    let td3 = document.createElement("td");
+    let td4 = document.createElement("td");
+    let td5 = document.createElement("td");
+    let td6 = document.createElement("td");
+    let td7 = document.createElement("td");
+    let td8 = document.createElement("td");
+
+    let pos = alunos.indexOf(aluno);
+    
+    
+    let numero = document.createTextNode(pos);
+    td1.appendChild(numero);  
+
+    
+    //colocando os inpuit
+    td3.innerHTML = `<input type= number name=notas class='media1 T ${pos}'  placeholder=nota>`
+    td4.innerHTML = `<input type= number name=notas class='media2 T ${pos}'  placeholder=nota>`
+    td5.innerHTML = `<input type= number name=notas class='media3 T ${pos}'  placeholder=nota>`
+    td7.innerHTML = `<input type= number name=notas class='media3 T ${pos}'  placeholder=nota>`
+    td8.innerHTML = `<input type= number name=notas class='media3 T ${pos}'  placeholder=nota>`
+    //calculando médias do Trimestre.
+    document.getElementById("media").addEventListener("click",()=>{
+        let nota1=parseInt(document.getElementsByClassName("nota "+ pos)[0].value);
+        let nota2=parseInt(document.getElementsByClassName("nota "+ pos)[1].value);
+        let nota3=parseInt(document.getElementsByClassName("nota "+ pos)[2].value);
+        let media = (nota1+nota2+nota3)/3;
+        td6.innerHTML = `<input type= number name=notas class='nota ${pos}'  placeholder=nota value = '${media}'>`
+    })
+    
+
+
+    //números e nomes dos alunos
+    td1.textContent = pos+1;
+    td2.textContent = nomeAluno.value;
+    tabelaRela.appendChild(tr);
+    tr.appendChild(td1);
+    tr.appendChild(td2);
+    tr.appendChild(td3);
+    tr.appendChild(td4);
+    tr.appendChild(td5);
+    tr.appendChild(td6);
+    tr.appendChild(td7);
+    tr.appendChild(td8);
+}
+
 
     document.getElementById("entrar").addEventListener("click",function(){
     document.getElementById("cabeca").style.display = "none";
@@ -138,7 +198,7 @@ document.getElementById("voltaPr").addEventListener("click", function(){
     document.getElementById("fre").style.display = "block"
 })
 
-document.getElementById("levaN").addEventListener("click",function(){
+document.getElementById("levaN").addEventListener("click",()=> {
     if(alunos==0){
         alert("Por favor, cadestre pelo menos um aluno!")
     }
@@ -147,23 +207,27 @@ document.getElementById("levaN").addEventListener("click",function(){
     document.getElementById("notas1").style.display = "block";
 })
 
-document.getElementById("Volta").addEventListener("click",function(){
+document.getElementById("Volta").addEventListener("click",()=> {
     document.getElementById("coisa1").style.display = "block";
     document.getElementById("notas").style.display = "none";
     document.getElementById("notas1").style.display = "none";
 })
 
-document.getElementById("Volta").addEventListener("click",function(){
+document.getElementById("Volta").addEventListener("click",()=>{
     document.getElementById("notas1").style.display = "none";
     document.getElementById("coisa1").style.display = "block";
 })
 
-document.getElementById("levaR").addEventListener("click",function(){
+document.getElementById("levaR").addEventListener("click",()=>{
+    if(alunos==0){
+        alert("Por favor, cadestre pelo menos um aluno!")
+    }
     document.getElementById("coisa1").style.display = "none";
     document.getElementById("rela").style.display = "block";
 })
 
-document.getElementById("ulti").addEventListener("click",function(){
+document.getElementById("ulti").addEventListener("click",()=>{
     document.getElementById("rela").style.display = "none";
     document.getElementById("coisa1").style.display = "block";  
 })
+document.getElementsByClassName("gerar")[0].addEventListener("click",()=> alert("Estamos trabalhando nesta funcionalidade"))
